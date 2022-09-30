2b0e7e94700e5a98a05dcd0b0424c5b5f0792902
jmauwerb <jeroen.werbrouck@ugent.be>
2022-09-30 17:12:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Demo page
+A demo Pilet. Can be used as a page, a tile or an extension. 
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { PiletApi } from 'consolid-shell';
+import {Link} from 'react-router-dom'
+
+export function setup(app: PiletApi) {
+  const connect = app.makeState(app)
+  const Module = connect(({state, actions}) => app.withState(App, {app, state, actions}))
+
+  app.showNotification('Registered Demo Page!', {
+    autoClose: 2000,
+  });
+  app.registerPage("/demo", Module)
+  app.registerMenu(() =>
+    <Link to="/demo">Demo</Link>
+  );
+}
